20bb4e099285ee98a24150499e342efeb42ed196
JonathonKang <jonkang@hotmail.com>
2017-04-07 09:10:17 -0700

    add submit button to form and a submit function to create pizza object via constructor and prototype functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 //Business Logic
 function Pizza(size,toppings){
-  this.size = size;
+  this.size = parseInt(size);
   this.toppings = toppings;
-  this.price = 10;
 }
 Pizza.prototype.price = function(){
-  //price modifiers
+  this.price = 10 + (this.size*2.5) + (this.toppings.length * .5);
 }
 
-
+//UI Logic
 $(document).ready(function(){
 
+  $('form').submit(function(event){
+    var size = $('#size').val();
+    var toppings = [];
+    $('input:checkbox:checked').each(function(){
+      var topping = $(this).val();
+      toppings.push(topping);
+    })
 
+    var newPizza = new Pizza(size,toppings);
+    newPizza.price();
+    console.log(newPizza);
+
+    event.preventDefault();
+  })
 
 });
